dc3a20231718d7a3775e7254708ed88981246d47
DDavitaa <c20406272@mytudublin.ie>
2023-10-06 15:57:03 +0100

    dodecahedrons

--- themetaverse/tank1.gd
+++ themetaverse/tank1.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 @export var move_speed:float=5
-@export var rot_speed:float=1
+@export var rot_speed:float=3
 
 func _process(delta):	
 	var move = Input.get_axis("move_backwards", "move_forwards")
@@ -9,6 +9,6 @@ func _process(delta):
 	
 	translate(Vector3.FORWARD * move * delta * move_speed)
 	
-	var turn = Input.get_axis("turn_left", "turn_right")
+	var turn = Input.get_axis("turn_right","turn_left")
 	
 	rotate(Vector3.UP, turn * rot_speed * delta)
