0c6a3e71035fac32cca24f486b17fb758bf6686a
DDavitaa <c20406272@mytudublin.ie>
2023-11-03 16:00:18 +0000

    check jet.gd for buttons

--- themetaverse/jet.gd
+++ themetaverse/jet.gd
@@ -18,9 +18,34 @@ func _physics_process(delta):
 		flames.emitting = false
 
 func _on_xr_controller_3d_input_float_changed(name, value):
-	if name == "trigger":
+	if name == "grip":
 		trigger =  value
+		
+	#print(name)
 #	
+	pass
+
+func _on_xr_controller_3d_button_pressed(name):
+	#print("PRESS LEFT:", name)
+	pass # Replace with function body.
+
+func _on_xr_controller_3d_button_released(name):
+	#print("RELEASE LEFT:", name)
+	pass # Replace with function body.
+
+func _on_xr_controller_3d_2_button_pressed(name):
+	#print("PRESS RIGHT:", name)
+	pass # Replace with function body.
+
+func _on_xr_controller_3d_2_button_released(name):
+	#print("PRESS RIGHT:", name)
+	pass # Replace with function body.
+
+func _on_xr_controller_3d_input_vector_2_changed(name, value):
+	print("LEFT:", name, " ", value)
 	pass # Replace with function body.
 
 
+func _on_xr_controller_3d_2_input_vector_2_changed(name, value):
+	print("RIGHT:", name, " ", value)
+	pass # Replace with function body.
